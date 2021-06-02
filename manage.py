0abe8a87625a61a5e5067fcab2facb1560dc63f3
Alamin5512 <56962724+Alamin5512@users.noreply.github.com>
2021-06-02 17:03:56 +0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,209 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as tt
+import torchvision.models as models
+from torchvision.datasets import ImageFolder
+from torchvision.utils import make_grid
+from torch.utils.data import random_split, DataLoader
+from torch.utils.data import  Dataset
+import jinja2
+from PIL import Image
+import io
 
+#device='cpu'
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device) # yield will stop here, perform other steps, and the resumes to the next loop/batch
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+def accuracy(outputs, labels):
+    output_probs = F.softmax(outputs, dim=1)
+    x_pred_prob =  (torch.max(output_probs.data, 1)[0][0]) * 100
+    _, preds = torch.max(outputs, dim=1) 
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds)), preds,x_pred_prob
+
+def F1_score(outputs, labels):
+    _, preds = torch.max(outputs, dim=1) 
+    
+    # precision, recall, and F1
+    cm  = confusion_matrix(labels, preds)
+    tn, fp, fn, tp = cm.ravel()
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    f1 = 2*((precision*recall)/(precision+recall))
+    
+    return precision,recall,f1,preds
+class PneumoniaModelBase(nn.Module):
+    
+    # this is for loading the batch of train image and outputting its loss, accuracy 
+    # & predictions
+    def training_step(self, batch, weight):
+        images,labels = batch
+        out = self(images)                                      # generate predictions
+        loss = F.cross_entropy(out, labels, weight=weight)      # weighted compute loss
+        acc,preds = accuracy(out, labels)                       # calculate accuracy
+        
+        return {'train_loss': loss, 'train_acc':acc}
+       
+    # this is for computing the train average loss and acc for each epoch
+    def train_epoch_end(self, outputs):
+        batch_losses = [x['train_loss'] for x in outputs]       # get all the batches loss
+        epoch_loss = torch.stack(batch_losses).mean()           # combine losses
+        batch_accs = [x['train_acc'] for x in outputs]          # get all the batches acc
+        epoch_acc = torch.stack(batch_accs).mean()              # combine accuracies
+        
+        return {'train_loss': epoch_loss.item(), 'train_acc': epoch_acc.item()}
+         # this is for loading the batch of val/test image and outputting its loss, accuracy, 
+    # predictions & labels
+    def validation_step(self, batch):
+        images,labels = batch
+        out = self(images)                                      # generate predictions
+        loss = F.cross_entropy(out, labels)                     # compute loss
+        acc,preds,prob = accuracy(out, labels)                # calculate acc & get preds 
+        return {'val_loss': loss.detach(), 'val_acc':acc.detach(), 
+               'preds':preds.detach(), 'labels':labels.detach(),'prob': prob.detach()}
+    # detach extracts only the needed number, or other numbers will crowd memory
+    
+    # this is for computing the validation average loss and acc for each epoch
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]         # get all the batches loss
+        epoch_loss = torch.stack(batch_losses).mean()           # combine losses
+        batch_accs = [x['val_acc'] for x in outputs]            # get all the batches acc
+        epoch_acc = torch.stack(batch_accs).mean()              # combine accuracies
+        
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+        # this is for printing out the results after each epoch
+    def epoch_end(self, epoch, train_result, val_result):
+        print('Epoch [{}], train_loss: {:.4f}, train_acc: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}'.
+              format(epoch+1, train_result['train_loss'], train_result['train_acc'],
+                     val_result['val_loss'], val_result['val_acc']))
+    
+    # this is for using on the test set, it outputs the average loss and acc, 
+    # and outputs the predictions
+    def test_prediction(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()           # combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()              # combine accuracies
+        batch_prob = [x['prob'] for x in outputs]
+        epoch_prob = torch.stack(batch_prob).mean()  
+        # combine predictions
+        batch_preds = [pred for x in outputs for pred in x['preds'].tolist()] 
+        # combine labels
+        batch_labels = [lab for x in outputs for lab in x['labels'].tolist()]  
+        
+        return {'test_loss': epoch_loss.item(), 'test_acc': epoch_acc.item(),
+                'test_preds': batch_preds, 'test_labels': batch_labels,'proba':epoch_prob.item()}
+resnet50 = models.resnet50(pretrained=True)
+#resnet50
+
+class PneumoniaResnet(PneumoniaModelBase):
+    def __init__(self):
+        super().__init__()
+        # Use a pretrained model
+        self.network = models.resnet50(pretrained=True)
+        # Freeze training for all layers before classifier
+        for param in self.network.fc.parameters():
+            param.require_grad = False  
+        num_features = self.network.fc.in_features # get number of in features of last layer
+        self.network.fc = nn.Linear(num_features, 2) # replace model classifier
+    
+    def forward(self, xb):
+        return self.network(xb)
+    
+#     def freeze(self):
+#         # To freeze the residual layers
+#         for param in self.network.parameters():
+#             param.require_grad = False
+#         for param in self.network.fc.parameters():
+#             param.require_grad = True
+    
+#     def unfreeze(self):
+#         # Unfreeze all layers
+#         for param in self.network.parameters():
+#             param.require_grad = True
+
+
+
+data_dir = r"/home/weevdlnc/pneupred2/static/chestx"
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = checkpoint['model']
+    model.load_state_dict(checkpoint['state_dict'])
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+
+    model.eval()
+    return model
+
+#model = load_checkpoint(r'C:\Users\ASUS !\Downloads\PneumoniaResnet.pth')
+
+
+@torch.no_grad()
+def test_predict(model, test_loader):
+    # perform testing for each batch
+    outputs = [model.validation_step(batch) for batch in test_loader] 
+    results = model.test_prediction(outputs)     
+
+
+    #print('test_loss: {:.4f}, test_acc: {:.4f}\nPrediction: {}'
+    #      .format(results['test_loss'], results['test_acc'],results['test_preds']))
+    #print(results['test_labels'])
+    return results['test_preds'], results['test_labels'],results['proba']
+
+
+my_transform=tt.Compose([tt.Resize(255),
+                         tt.CenterCrop(224),                                                              
+                         tt.ToTensor()
+                                                 #tt.Normalize(mean=[0.485, 0.456, 0.406], 
+                                                 #             std=[0.229, 0.224, 0.225],
+                                                 #             inplace=True)
+                         ])
+
+UPLOAD=r"/home/weevdlnc/pneupred2/static/chestx/test/NORMAL"
+# Plot Accuracy and Loss 
+def predict(data_dir):
+ model=load_checkpoint(r'PneumoniaResnet.pth')
+ test_dataset = ImageFolder(data_dir+'/test',
+                                 transform=tt.Compose([tt.Resize(255),
+                                                       tt.CenterCrop(224),                                                              
+                                                       tt.ToTensor()
+                                                 #tt.Normalize(mean=[0.485, 0.456, 0.406], 
+                                                 #             std=[0.229, 0.224, 0.225],
+                                                 #             inplace=True)
+                                               ]))
+
+ 
+
+# Evaluate test set
+ device='cpu'
+ test_dl = DataLoader(test_dataset, batch_size=1)
+ test_dl = DeviceDataLoader(test_dl, device)
+ preds,labels,proba = test_predict(model, test_dl)
+ preds,labels,proba= test_predict(model, test_dl)
+ return preds,proba
 
 def main():
     """Run administrative tasks."""
@@ -19,4 +221,5 @@ def main():
 
 
 if __name__ == '__main__':
+    model = load_checkpoint('/home/weevdlnc/pneupred2/PneumoniaResnet.pth')
     main()
